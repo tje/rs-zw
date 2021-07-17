e49e0f9980ace6168e880450cd2c7f299cbc83e4
modem <tyler@modem.io>
2021-07-17 08:36:16 -0700

    reindent auto bin to 4sp for consistency

--- src/bin/auto.rs
+++ src/bin/auto.rs
@@ -5,41 +5,41 @@ use atty;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
-  #[structopt(short = "e", long = "encode")]
-  encode: bool,
-  #[structopt(short = "d", long = "decode")]
-  decode: bool,
-  input: Option<String>,
+    #[structopt(short = "e", long = "encode")]
+    encode: bool,
+    #[structopt(short = "d", long = "decode")]
+    decode: bool,
+    input: Option<String>,
 }
 
 pub fn main() -> io::Result<()> {
-  let args = Cli::from_args();
-  let input: String = match args.input {
-    Some(s) => s,
-    None if atty::isnt(atty::Stream::Stdin) => {
-      let mut buf = String::new();
-      io::stdin().read_to_string(&mut buf)?;
-      buf
-    },
-    _ => {
-      eprintln!("Error: no input provided");
-      std::process::exit(0x0100);
-    },
-  };
-
-  let input = input.trim();
-  if args.encode {
-    println!("{}", zw::encode(input));
-  } else if args.decode {
-    println!("{}", zw::decode(input));
-  } else {
-    let ch = input.chars().next().unwrap();
-    let out = match ch {
-      zw::ZW_0 | zw::ZW_1 => zw::decode(input),
-      _ => zw::encode(input),
+    let args = Cli::from_args();
+    let input: String = match args.input {
+        Some(s) => s,
+        None if atty::isnt(atty::Stream::Stdin) => {
+            let mut buf = String::new();
+            io::stdin().read_to_string(&mut buf)?;
+            buf
+        },
+        _ => {
+            eprintln!("Error: no input provided");
+            std::process::exit(0x0100);
+        },
     };
-    println!("{}", out);
-  }
 
-  Ok(())
+    let input = input.trim();
+    if args.encode {
+        println!("{}", zw::encode(input));
+    } else if args.decode {
+        println!("{}", zw::decode(input));
+    } else {
+        let ch = input.chars().next().unwrap();
+        let out = match ch {
+            zw::ZW_0 | zw::ZW_1 => zw::decode(input),
+            _ => zw::encode(input),
+        };
+        println!("{}", out);
+    }
+
+    Ok(())
 }
